b834698360f0bae898f2f0361f95b33b74692aaf
Marcos Wilson <mwilson.oliveira@gmail.com>
2023-11-21 18:57:23 -0300

    Routes working

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,3 @@
 <template>
-  <el-dialog v-model="openDialog" title="Tips" width="30%" :before-close="handleClose">
-    <span>This is a message</span>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="openDialog = false">Cancel</el-button>
-        <el-button type="primary" @click="handleClose"> Confirm </el-button>
-      </span>
-    </template>
-  </el-dialog>
-
-  <el-row>
-    <el-col :span="24">
-      <div class="grid-content ep-bg-purple-dark">
-        <CustomButton @click="openDialog = true"> Custom Button </CustomButton>
-      </div>
-    </el-col>
-  </el-row>
+  <router-view />
 </template>
-
-<script lang="ts" setup>
-import { ElMessageBox } from 'element-plus'
-import { ref } from 'vue'
-import CustomButton from './components/CustomButton.vue'
-
-const openDialog = ref(false)
-
-const handleClose = () => {
-  ElMessageBox.confirm('Are you sure to close this dialog?')
-    .then(() => {
-      openDialog.value = false
-    })
-    .catch(() => {
-      // catch error
-    })
-}
-</script>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 import './styles/index.css'
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.use(router)
 app.mount('#app')
