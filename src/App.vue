9581c2ae404f9bd1d421a65bb7c67efe1410ff13
Marcos Wilson <mwilson.oliveira@gmail.com>
2023-11-21 10:54:46 -0300

    Setting up font-family

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import { ElMessageBox } from 'element-plus'
+import { ref } from 'vue'
+
+const openDialog = ref(false)
+
+const handleClose = (done: () => void) => {
+  ElMessageBox.confirm('Are you sure to close this dialog?')
+    .then(() => {
+      done()
+    })
+    .catch(() => {
+      // catch error
+    })
+}
+</script>
 
 <template>
+  <el-dialog v-model="openDialog" title="Tips" width="30%" :before-close="handleClose">
+    <span>This is a message</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="openDialog = false">Cancel</el-button>
+        <el-button type="primary" @click="openDialog = false"> Confirm </el-button>
+      </span>
+    </template>
+  </el-dialog>
+
   <el-row>
     <el-col :span="24">
       <div class="grid-content ep-bg-purple-dark">
-        <el-button>teste</el-button>
+        <el-button @click="openDialog = true">teste</el-button>
       </div>
     </el-col>
   </el-row>
