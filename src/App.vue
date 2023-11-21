ec4866ddb8bd66d56c1edbb78347491140191460
Marcos Wilson <mwilson.oliveira@gmail.com>
2023-11-21 10:33:57 -0300

    First implementation of element-plus working

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <script setup lang="ts"></script>
 
-<template>teste</template>
+<template>
+  <el-row>
+    <el-col :span="24">
+      <div class="grid-content ep-bg-purple-dark">
+        <el-button>teste</el-button>
+      </div>
+    </el-col>
+  </el-row>
+</template>
